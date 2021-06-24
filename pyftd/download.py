1fc2fd9a7c22c3167bd664390958db1bcd111c0d
aaronhackney <ahack210@gmail.com>
2021-06-24 09:26:51 -0500

    download

---
+++ pyftd/download.py
@@ -0,0 +1,13 @@
+import logging
+from .base import FTDAPIWrapper
+from typing import Optional
+
+log = logging.getLogger(__name__)
+
+
+class FTDDownload:
+    """
+    Download various files from the FTD Appliance
+    """
+
+    pass
